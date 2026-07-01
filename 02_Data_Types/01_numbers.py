c678850ed145993abfa7570917c9972b939ac7e0
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-01 14:32:15 +0530

    Day-02 : Add Numbers data type

---
+++ 02_Data_Types/01_numbers.py
@@ -0,0 +1,8 @@
+age = 22   #integer data type
+height = 5.8  # decimal data type
+complex_number = 3 + 4j  #complex number data type
+print(age)
+print(height)
+print(complex_number)
+
+""" data type is a way of classified data """
