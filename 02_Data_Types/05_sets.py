edce26ac70028a9a94ef1913ccefd7e6e9900545
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 15:24:01 +0530

    Add sets in python

---
+++ 02_Data_Types/05_sets.py
@@ -0,0 +1,8 @@
+fruits = {"Apple", "Banana", "Mango", "Orange"}
+print(fruits)
+
+# same as list or tuple but we donnt make duplicate values 
+numbers = {10,10,20,30,40,20,20,50}
+print(numbers)
+ # unfixed order
+ 
