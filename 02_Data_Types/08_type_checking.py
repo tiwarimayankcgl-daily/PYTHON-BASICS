830629acee4a927ed4c5962d5ded8d0f0e634494
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 15:51:07 +0530

    Add type checking in python repo

---
+++ 02_Data_Types/08_type_checking.py
@@ -0,0 +1,11 @@
+name = "Mayank"
+age = 22
+height = 5.7
+is_student = True
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+
+# type() is a built-in-function 
+# in this variable  we checked data type of any variables
