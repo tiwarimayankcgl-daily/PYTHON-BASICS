ea58d20cc74f5f96d9cbe4deb0b42d07a399363b
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 15:36:12 +0530

    Add dictonary in repo

---
+++ 02_Data_Types/06_dictionaries.py
@@ -0,0 +1,12 @@
+students = {
+    "name" : "Mayank",
+    "age" : 22,
+    "course" : "MCA",
+    "collage" : "Vgu",
+    "marks" : 78.5,
+    }
+print(students)
+
+# IN Dictonary we have key : value
+# set only values
+# Dictonary key  and value
