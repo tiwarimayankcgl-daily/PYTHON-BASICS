66e8db0d8a0cdcbd66f38d7fd71777bbb3d7b75d
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 15:43:42 +0530

    Add boolean in the repo

---
+++ 02_Data_Types/07_boolean.py
@@ -0,0 +1,7 @@
+is_student = True
+is_graduate = False
+print(is_student)
+print(is_graduate)
+
+# Boolean is a data type in the we have only 2 value True or False
+# Boolean used in the decision making
