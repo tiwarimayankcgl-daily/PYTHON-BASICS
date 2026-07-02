2f0e52d88509ca01667713df98db8a194a23e79c
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 15:07:38 +0530

    Add lists data type

---
+++ 02_Data_Types/03_lists.py
@@ -0,0 +1,10 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print(fruits)
+
+#list is a data type ehich is stored muiltiple values in a single variables
+#[] list
+
+# diff data type in the list
+
+data = ["Mayank", 22 , 8.5, True]
+print(data)
