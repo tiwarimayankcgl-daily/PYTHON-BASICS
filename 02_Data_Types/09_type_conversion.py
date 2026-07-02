1ff84c2c3fec226790283f24c38ca35e4b9adcf7
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 16:02:12 +0530

    Add type conversion in repo

---
+++ 02_Data_Types/09_type_conversion.py
@@ -0,0 +1,15 @@
+# String to integer
+age = "22"
+print(age)
+print(type(age))
+age = int(age)
+print(age)
+print(type(age))
+ # data type change in the type  coversion
+
+age = input("Enter your age :")
+print(type(age))
+age =int(age)
+print(type(age))
+print("Next year Age =", age + 1)
+ 
