bd4a5cb9693926ce6ad259443e62485791d44cc4
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-02 16:57:43 +0530

    Add some practice questions for day-2

---
+++ 02_Data_Types/10_practice_questions.py
@@ -0,0 +1,127 @@
+# IN THIS SECTIONN WE HAVE SOME PRACTICE QUESTIONS
+
+#NUMBERS 
+
+# 1
+age = 22
+height = 5.7
+weight = 65
+print(age, height, weight)
+
+#2
+marks = 95
+temperature = 36.5
+complex_number = 7 + 3j
+print(marks)
+print(temperature)
+print(complex_number)
+
+# STRINGS
+#1
+name = "MAYANK"
+collage = "VGU"
+course = "MCA"
+city = "BIHAR"
+print(name , collage, course, city)
+
+#2
+favourite_language = 'English'
+favourite_IDE = "I DONT KNOW WHAT IS IDE"
+favourite_company = "Micro soft and google"
+print(favourite_language)
+print(favourite_company)
+print(favourite_IDE)
+
+#LISTS
+#1
+programminng_lannguage = ["Python", "C", "Java", "C++", "R"]
+print(programminng_lannguage)
+
+#2
+favourite_fruits = ["Apple", "Mango", "Orange", "Guava", "Banana"]
+print(favourite_fruits)
+
+#TUPLES
+#1
+laptop_details = ("Lenvo", "Samsung", "Bihar", "India")
+print(laptop_details)
+
+#2
+students = ("Roll_number :46546", "Name :Mayank", "course :MCA")
+print(students)
+
+#SETS
+Data_analytics_skill = {"Python", "SQL", "Excel", "Python", "Power BI", "SQL"}
+print(Data_analytics_skill)
+
+#2
+
+numbers ={10,20,30,20,40,10}
+print(numbers)
+
+#DICTIONARIES
+
+students = {
+    "Name" :"MAYANK",
+    "age" : 22,
+    "collage" :"VGU JAIPUR",
+    "Course" :"MCA"
+}
+print(students)
+
+#2 
+
+laptop_details= {
+    "Brand" :"lenevo",
+    "Model" : "len_7785i",
+    "price" : 48000
+}
+print(laptop_details)
+
+#BOOLEAN
+
+is_student = True
+is_python_easy = True
+print(is_student, is_python_easy)
+
+#2
+has_job = False
+has_laptop = True
+is_indian = True
+print(has_job, has_laptop, is_indian)
+
+# TYPE CHECKING
+#1
+
+name= "Mayank"
+Age = 22
+CGPA = 7.5
+Is_placed = False
+print(type(name))
+print(type(Age))
+print(type(CGPA))
+print(type(Is_placed))
+
+#2
+Country = 'INDIA'
+Salary = 25000
+height = 5.7
+print(type(Country))
+print(type(Salary))
+print(type(height))
+
+
+# TYPE CONVERSION
+#1
+
+numbers = "100"
+print(type(numbers))
+numbers =int(numbers)
+print(type(numbers))
+
+#2
+numbers = 50
+print(type(numbers))
+numbers =float(numbers)
+print(type(numbers))
+
