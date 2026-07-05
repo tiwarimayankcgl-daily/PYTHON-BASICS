de3ba01fe74fc00c79b0db1ca271c0c3b9f0c42d
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 19:58:05 +0530

    Add identity operator

---
+++ 03_Opearators/05_identity_operators.py
@@ -0,0 +1,11 @@
+x = 10
+y = 10
+z = 20
+print( x is y)
+print( x is z)
+print( x is not y)
+print( x is not y)
+
+# Used for compare identity
+# is =  both r same object
+# is not = both are diffrent object
