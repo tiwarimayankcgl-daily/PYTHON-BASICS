983fb2a41fc17c74d5a7bf8c28b3c69e547f92f7
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 19:27:22 +0530

    Add assignment operator

---
+++ 03_Opearators/02_assignment_operators.py
@@ -0,0 +1,9 @@
+# Assignment operator kisi variable me assign value or value update karta hai
+
+a = 10
+print("Initial value is :", a)
+a += 3
+print("After add +=", a)
+a -= 3
+print("After substract", a)
+
