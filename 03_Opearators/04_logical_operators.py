53fb6b35d6aaf26b234c04716c8082194bdd7af1
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 19:50:21 +0530

    Add logicl opertor

---
+++ 03_Opearators/04_logical_operators.py
@@ -0,0 +1,11 @@
+a = 10
+b = 20
+print(a < b and b > 15)
+print(a > b and b > 15)
+print(a > b or b > 15)
+print(a > b or b < 15)
+print(not(a < b))
+print(not(a > b))
+
+# if we need to check more than one condition than we used logical operator
+# and , or , not(make oppiset result)
