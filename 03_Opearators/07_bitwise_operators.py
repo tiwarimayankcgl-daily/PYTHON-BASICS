4db1b752e62bb17d189123086e0261d88ac30c64
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 20:32:29 +0530

    Add bitwise operator

---
+++ 03_Opearators/07_bitwise_operators.py
@@ -0,0 +1,10 @@
+a = 5
+b = 3
+print("AND :", a & b)
+print("OR :", a & b)
+print("XOR :", a ^ b)
+print("NOT :", ~a)
+print("LEFT SHIFT:", a << b)
+print("RIGHT SHIFT:", a >> b)
+
+# PRINT BINARY VALUE
