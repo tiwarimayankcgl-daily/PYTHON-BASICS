b040875ecdfa4db231ca64e0974d9bedd4011e49
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 13:06:15 +0530

    Add arithemetics operators

---
+++ 03_Opearators/01_arithemetic_operators.py
@@ -0,0 +1,13 @@
+a = float(input("Enter a number: "))
+b = float(input("Enter a number: "))
+ 
+print(f"Addition of two number is : {a+b} ")
+print(f"Substraction of two number is : {a-b}")
+print(f"Multiplication of two number is : {a*b}")
+print(f"Divison of two number is : {a/b}")
+print(f"Flour division of two number is : {a//b}")
+print(f"Modules of two number is : {a%b}")
+print(f"Exponent of two number is : {a**b}")
+
+# Operator is a symbol which is perform task
+# + ,-,*,/,//,** 
