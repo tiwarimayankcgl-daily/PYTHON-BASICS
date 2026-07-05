546ee3db7d04f1677e15c2b313edc85d4d2fd9d3
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 20:22:10 +0530

    Add membership operator

---
+++ 03_Opearators/06_membership_operators.py
@@ -0,0 +1,8 @@
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print("Apple" in fruits)
+print("Grapes" in fruits)
+print("Apple" not in fruits)
+print("Grapes" not in fruits)
+
+# Checked value in list,tuple,set,dictonary,strings 
+# in , not in
