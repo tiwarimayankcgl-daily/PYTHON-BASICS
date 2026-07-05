538b419bad1e31e0a7ef5bc819d961d6b3b5b7b1
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-05 19:40:24 +0530

    Add comprasion operators

---
+++ 03_Opearators/03_comparison_operators.py
@@ -0,0 +1,12 @@
+a = 10
+b = 20
+
+print("Equal :", a == b)
+print("Not Equal :", a !=b)
+print("Greater than :", a >b)
+print("Less Than :", a < b)
+print("Greater than or Equal :", a >= b)
+print("Less than or Equal :", a <= b)
+
+# Compare two values and return True or False
+# == , !=, <,>, >=, <= (6 comprasion operators)
