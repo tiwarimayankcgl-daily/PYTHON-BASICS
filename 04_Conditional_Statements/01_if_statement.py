d717d303a388d1a4b338532351cb8a2e68affb3e
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-06 15:32:05 +0530

    Add if statement in the conditional statement

---
+++ 04_Conditional_Statements/01_if_statement.py
@@ -0,0 +1,8 @@
+age = int(input("Enter your age :"))
+if age >= 18:
+    print("You are eligible to give vote ")
+    
+# If statement is used to make decision in python.
+# only excuted when condition is true
+# python uses indentation to know which is belongs to the if block
+    
