f78e3d8a687f4ebf55a46fb25e2443897a0aa911
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-06 15:39:41 +0530

    Add if-else condition

---
+++ 04_Conditional_Statements/02_if_else.py
@@ -0,0 +1,9 @@
+age = int(input("Enter Your age :"))
+if age >= 18:
+    print("You are eligible to give vote.")
+else:
+    print("You are not aligible to vote.")
+    
+#if- else we used when we need to handle 2 diffrent situation..
+# if when condition is True , Else when condition is false
+
