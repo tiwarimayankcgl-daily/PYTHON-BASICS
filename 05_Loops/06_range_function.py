6e52f690d6d235fd2b142d927842f5a22d9871cc
tiwarimayankcgl-daily <tiwarimayankcgl@gmail.com>
2026-07-13 12:44:43 +0530

    Add range() function in details.

--- 05_Loops/06_range_function.py
+++ 05_Loops/06_range_function.py
@@ -1,2 +1,19 @@
 # Range function lets start
 print("Range function :")
+
+for i in range(5):
+    print(i)
+    
+# 5 not printed.
+# start 0 , end exclude given number
+
+for i in range(1, 6):
+    print(i)
+
+# start , stop , step
+for i in range(1, 100 , 2):
+    print(i)
+    
+# Reverse counting 
+for i in range(10, 0, -1):
+    print(i)
